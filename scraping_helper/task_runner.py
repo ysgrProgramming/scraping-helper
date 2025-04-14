1d399a667c5e245bf5293d5e6e615bd17b83c77d
magnate <y.mizutani_1205@icloud.com>
2025-04-14 10:44:51 +0900

    Create test_run.py

--- scraping_helper/task_runner.py
+++ scraping_helper/task_runner.py
@@ -1,60 +1,105 @@
 import logging
 from collections import deque
-from dataclasses import dataclass, field
 from logging import getLogger
 from pathlib import Path
-
+from pydantic import Field, BaseModel
 from .checkpoint_recorder import CheckpointRecorder
 from .task import Task
 
 logger = getLogger(__name__)
 
 
-@dataclass
-class TaskRunnerConfig:
-    checkpoint_path: Path = field(default=Path("scraping_helper.ckpt"))
-    retry_count: int = field(default=-1)
+class TaskRunnerConfig(BaseModel):
+    checkpoint_path: Path = Field(default=Path("scraping_helper.ckpt"))
+    retry_count: int = Field(default=0, ge=-1)
+    ignore_error: bool = Field(default=False)
 
-    start_logging_level: int = field(default=logging.INFO)
-    done_logging_level: int = field(default=logging.INFO)
-    skip_logging_level: int = field(default=logging.DEBUG)
-    error_logging_level: int = field(default=logging.ERROR)
+    start_logging_level: int = Field(default=logging.INFO)
+    done_logging_level: int = Field(default=logging.INFO)
+    skip_logging_level: int = Field(default=logging.DEBUG)
+    error_logging_level: int = Field(default=logging.ERROR)
 
 
-@dataclass
-class TaskRunner:
+class TaskNode(BaseModel):
+    task: Task
+    children: list["TaskNode"] | None = Field(default=None)
+    parent: "TaskNode" | None = Field(default=None)
+    children_cursor: int = 0
+
+    def next_child(self):
+        if self.children is None or len(self.children) <= self.children_cursor:
+            return None
+        child = self.children[self.children_cursor]
+        self.children_cursor += 1
+        return child
+
+
+def resolve_value(preferred_value, default_value):
+    if preferred_value is None:
+        return default_value
+    else:
+        return preferred_value
+
+
+class TaskRunner(BaseModel):
     """タスクを実行するクラス"""
 
-    config: TaskRunnerConfig = field(default_factory=TaskRunnerConfig)
+    config: TaskRunnerConfig = Field(default_factory=TaskRunnerConfig)
 
     def run(self, *args: Task) -> None:
         checkpoint_recorder = CheckpointRecorder(
             checkpoint_path=self.config.checkpoint_path,
         )
-        tasks = deque(args)
-        while tasks:
-            task = tasks.popleft()
+        nodes = deque(TaskNode(task=task) for task in args)
+        while nodes:
+            node = nodes.popleft()
+            task = node.task
             if checkpoint_recorder.exists(task.uid):
+                skip_logging_level = resolve_value(
+                    task.config.skip_logging_level, self.config.skip_logging_level
+                )
                 logger.log(
-                    self.config.skip_logging_level,
+                    skip_logging_level,
                     "%s は既に記録済みです。",
                     task.uid,
                 )
+                if node.parent is not None:
+                    brother = node.next_child()
+                    if brother is not None:
+                        nodes.appendleft(brother)
                 continue
             try:
-                logger.log(self.config.start_logging_level, "%s を開始します", task.uid)
+                start_logging_level = resolve_value(
+                    task.config.start_logging_level, self.config.start_logging_level
+                )
+                logger.log(start_logging_level, "%s を開始します", task.uid)
                 children = task.run()
+
+                if node.parent is not None:
+                    brother = node.parent.next_child()
+                    if brother is not None:
+                        nodes.appendleft(brother)
                 if children:
-                    tasks.extendleft(children)
+                    children_nodes = [
+                        TaskNode(task=task, parent=node) for task in children
+                    ]
+                    node.children = children_nodes
+                    nodes.appendleft(node.next_child())
                 checkpoint_recorder.record(task.uid)
+                done_logging_level = resolve_value(
+                    task.config.done_logging_level, self.config.done_logging_level
+                )
                 logger.log(
-                    self.config.start_logging_level,
+                    done_logging_level,
                     "%s が完了しました",
                     task.uid,
                 )
             except Exception as e:  # noqa: BLE001
+                error_logging_level = resolve_value(
+                    task.config.error_logging_level, self.config.error_logging_level
+                )
                 logger.log(
-                    self.config.error_logging_level,
+                    error_logging_level,
                     "%sでエラー発生: %s",
                     task.uid,
                     e,
--- test/unit_tests/test_task.py
+++ test/unit_tests/test_task.py
@@ -1,8 +1,8 @@
-from dataclasses import dataclass
 from scraping_helper import Task
 from pathlib import Path
+import tempfile
 
-@dataclass
+
 class MyTask(Task):
     a: int
     b: int
@@ -10,6 +10,8 @@ class MyTask(Task):
 
     def run(self):
         c = self.a + self.b
+        with self.save_path.open("w", encoding="utf-8") as f:
+            f.write(f"{self.a} + {self.b} = {c}")
 
 
 def test_mytask_init():
@@ -19,3 +21,13 @@ def test_mytask_init():
     assert mytask.a == 2
     assert mytask.b == 3
     assert mytask.save_path == Path("tmp")
+
+
+def test_mytask_run():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        save_path = Path(tmpdir) / "save.txt"
+        mytask = MyTask(name="mytask", a=2, b=3, save_path=save_path)
+        mytask.run()
+        with save_path.open("r", encoding="utf-8") as f:
+            msg = f.read()
+            assert msg == "2 + 3 = 5"
